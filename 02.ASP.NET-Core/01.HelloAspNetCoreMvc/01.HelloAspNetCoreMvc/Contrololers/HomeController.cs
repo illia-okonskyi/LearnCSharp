214b9a6dba70c4accd4433dadb060a5ea1ed3ffc
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2018-12-09 16:41:50 +0200

    [MVC.01] Added HelloAspNetCoreMvc project

---
+++ 02.ASP.NET-Core/01.HelloAspNetCoreMvc/01.HelloAspNetCoreMvc/Contrololers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAspNetCoreMvc.Contrololers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(Models.Data.GetHelloWorldSet());
+    }
+}
---
+++ 02.ASP.NET-Core/01.HelloAspNetCoreMvc/01.HelloAspNetCoreMvc/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace HelloAspNetCoreMvc.Models
+{
+    public class Data
+    {
+        public string Text { get; set; }
+
+        public static IEnumerable<Data> GetHelloWorldSet()
+        {
+            return new Data[] {
+                new Data { Text = "Hello"},
+                new Data { Text = "ASP.NET Core MVC" }
+            };
+        }
+    }
+}
