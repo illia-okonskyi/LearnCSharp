1137d34f3978d982e36b44739d9c55b0ea44b4a1
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2018-12-09 20:24:31 +0200

    [MVC.01] Added simple Razor features demo

--- 02.ASP.NET-Core/01.HelloAspNetCoreMvc/01.HelloAspNetCoreMvc/Models/Data.cs
+++ 02.ASP.NET-Core/01.HelloAspNetCoreMvc/01.HelloAspNetCoreMvc/Models/Data.cs
@@ -5,12 +5,14 @@ namespace HelloAspNetCoreMvc.Models
     public class Data
     {
         public string Text { get; set; }
+        public int Count { get; set; } = 1;
 
         public static IEnumerable<Data> GetHelloWorldSet()
         {
             return new Data[] {
                 new Data { Text = "Hello"},
-                new Data { Text = "ASP.NET Core MVC" }
+                new Data { Text = "ASP.NET Core MVC", Count = 2},
+                new Data { Text = "!", Count = 3}
             };
         }
     }
