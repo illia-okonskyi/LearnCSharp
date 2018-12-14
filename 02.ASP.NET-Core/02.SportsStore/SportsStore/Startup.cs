cd58a6bde60093e0f461aea2e1d13f3e42ada21e
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2018-12-14 16:42:11 +0200

    [MVC.02] Added initial project structure

---
+++ 02.ASP.NET-Core/02.SportsStore/SportsStore/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IProductRepository, DummyProductRepository>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
+            });
+        }
+    }
+}
