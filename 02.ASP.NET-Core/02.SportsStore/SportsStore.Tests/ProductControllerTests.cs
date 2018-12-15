3657b2a5ba02047426117237e9609904958b787d
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2018-12-15 13:17:10 +0200

    [MVC.02] Added pagination feature

---
+++ 02.ASP.NET-Core/02.SportsStore/SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Xunit;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void CanPaginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {Id = 1, Name = "P1"},
+                new Product {Id = 2, Name = "P2"},
+                new Product {Id = 3, Name = "P3"},
+                new Product {Id = 4, Name = "P4"},
+                new Product {Id = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            var productController = new ProductController(mock.Object)
+            {
+                PageSize = 3
+            };
+
+            var result = productController.List(2).ViewData.Model as ProductListViewModel;
+
+            Assert.NotNull(result);
+
+            var products = result.Products.ToArray();
+            var pagingInfo = result.PagingInfo;
+
+            Assert.Equal(2, products.Length);
+            Assert.Equal(4, products[0].Id);
+            Assert.Equal(5, products[1].Id);
+            Assert.Equal(2, pagingInfo.CurrentPage);
+            Assert.Equal(3, pagingInfo.ItemsPerPage);
+            Assert.Equal(5, pagingInfo.TotalItems);
+            Assert.Equal(2, pagingInfo.TotalPages);
+        }
+    }
+}
--- 02.ASP.NET-Core/02.SportsStore/SportsStore/Controllers/ProductController.cs
+++ 02.ASP.NET-Core/02.SportsStore/SportsStore/Controllers/ProductController.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductRepository _repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             _repository = repository;
         }
 
-        public ViewResult List() => View(_repository.Products);
+        public ViewResult List(int productPage = 1)
+        {
+            productPage = Math.Max(productPage, 1);
+
+            return View(new ProductListViewModel
+            {
+                Products = _repository.Products
+                    .OrderBy(p => p.Id)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            });
+        }
     }
 }
