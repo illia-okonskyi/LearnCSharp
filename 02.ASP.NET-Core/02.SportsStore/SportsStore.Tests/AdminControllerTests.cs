3e437e85ca03ae8c1f8265fe6ab0b83ba2f3f753
Illia Okonskyi <illia.okonskyi@developex.net>
2019-01-10 19:49:59 +0200

    [MVC.02] Added list (index) part of the product CRUD feature

---
+++ 02.ASP.NET-Core/02.SportsStore/SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using SportsStore.Models;
+using SportsStore.Controllers;
+
+namespace SportsStore.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void Index_ExactProductList()
+        {
+            Product [] expectedProducts = {
+                new Product {Id = 1},
+                new Product {Id = 2},
+                new Product {Id = 3}
+            };
+
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(expectedProducts.AsQueryable<Product>());
+            var adminController = new AdminController(mock.Object);
+
+            var actualProducts = GetViewModel<IEnumerable<Product>>(adminController.Index());
+            Assert.Equal(expectedProducts, actualProducts);
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
---
+++ 02.ASP.NET-Core/02.SportsStore/SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public AdminController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public ViewResult Index() => View(_productRepository.Products);
+    }
+}
