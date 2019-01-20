f910b648a6123175af629629790be1b1ba461242
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-01-20 22:08:28 +0200

    [MVC.04] Added UrlsAndRoutes project

---
+++ 02.ASP.NET-Core/04.UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+    }
+}
