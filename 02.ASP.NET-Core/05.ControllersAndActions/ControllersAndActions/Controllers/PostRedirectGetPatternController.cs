5c2b58876b2728b6957d15eb819eb0ad061e0e0e
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-01-26 18:42:20 +0200

    [MVC.05] Added exampe of the Post-Redirect-Get pattern

---
+++ 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/PostRedirectGetPatternController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    // NOTE: Main idea is to implement Post-Redirect-Get pattern to avoid situations of mutliply
+    //       posting forms and other data which are sent via POST request.
+    //       Example of such case: user fills form, submits it and action method returns view.
+    //       Than user clicks "Reload" button of the browser and browser posts data once more.
+    // NOTE: TempData which is used to share data across requests relies on the Session service
+    public class PostRedirectGetPatternController : Controller
+    {
+        public ViewResult Index() => View("SimpleForm");
+
+        [HttpPost]
+        public RedirectToActionResult ReceiveForm(string name, string city)
+        {
+            TempData["name"] = name;
+            TempData["city"] = city;
+            return RedirectToAction(nameof(ShowResult));
+        }
+
+        public ViewResult ShowResult()
+        {
+            string name = TempData["name"] as string;
+            string city = TempData["city"] as string;
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
