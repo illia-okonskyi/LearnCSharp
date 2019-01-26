697bd0f54e213ef1a06f661957a43b681b70cf76
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-01-26 14:03:06 +0200

    [MVC.05] Aded POCO controller

---
+++ 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ControllersAndActions.Controllers
+{
+    // NOTE: Apply attribute [Microsoft.AspNetCore.Mvc.NonController] for POCO which ends on
+    //       "Controller" and are not controllers.
+    //       Apply [Microsoft.AspNetCore.Mvc.NonAction] on public methods of the controller to
+    //       identify them as simple pulbic methods, not actions
+    public class PocoController
+    {
+        public string Index() => "This is a POCO controller";
+
+        public ViewResult Index2() => new ViewResult()
+        {
+            ViewName = "Result",
+            ViewData = new ViewDataDictionary(
+                new EmptyModelMetadataProvider(),
+                new ModelStateDictionary())
+            {
+                Model = $"This is a POCO controller"
+            }
+        };
+    }
+}
