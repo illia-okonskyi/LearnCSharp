f7dce8d0129e8683ff59d683a1a5069eeade04d5
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-01-26 19:13:17 +0200

    [MVC.05] Added example of sending JSON, custom object and files as action result

--- 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
+++ 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
@@ -25,8 +25,9 @@ namespace ControllersAndActions.Controllers
         //          there is no suchcontroller
         public RedirectResult RedirectExample() => Redirect("https://www.google.com");
         public LocalRedirectResult LocalRedirectExample() => LocalRedirect("/Home/Index");
-        public RedirectToRouteResult RedirectToRouteExample() => 
-            RedirectToRoute(new {
+        public RedirectToRouteResult RedirectToRouteExample() =>
+            RedirectToRoute(new
+            {
                 controller = "Home",
                 action = "ReceiveForm",
                 name = "Alice",
@@ -34,5 +35,36 @@ namespace ControllersAndActions.Controllers
             });
         public RedirectToActionResult RedirectToActionExample() =>
             RedirectToAction(nameof(DerivedController.Headers), "Derived");
+
+        public JsonResult JsonResultExample()
+            => Json(new
+            {
+                stringValue = "stringValue",
+                arrayValue = new[] { "Alice", "Bob", "Joe" }
+            });
+
+        public ContentResult ContentResultExample()
+            => Content("[\"Alice\",\"Bob\",\"Joe\"]", "application/json");
+
+        public ObjectResult ObjectResultExample() =>
+            Ok(new
+            {
+                stringValue = "stringValue",
+                arrayValue = new[] { "Alice", "Bob", "Joe" }
+            });
+
+        // NOTE: There are four IActionResult which can be used to return files
+        //       1) FileContentResult (accessed via File method of the MVC Controller)
+        //          This action result sends a byte array to the client with a specified MIME type.
+        //       2) FileStreamResult (accessed via File method of the MVC Controller)
+        //          This action result reads a stream and sends the content to the client.
+        //       3) VirtualFileResult (accessed via File method of the MVC Controller)
+        //          This action result reads a stream from a virtual path (relative to the
+        //          application on the host).
+        //       4) PhysicalFileResult (accessed via PhysicalFile method of the MVC Controller)
+        //          This action result reads the contents of a file from a specified path and sends
+        //          the contents to the client.
+        public VirtualFileResult FileResultExample()
+            => File("/lib/twitter-bootstrap/css/bootstrap.css", "text/css");
     }
 }
