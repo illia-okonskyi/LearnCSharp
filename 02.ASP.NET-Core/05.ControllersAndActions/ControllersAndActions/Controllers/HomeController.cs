4971380d9149a98019c27c21873ef7f5ed5521fa
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-01-26 13:41:14 +0200

    [MVC.05] Added ControllersAndActions project

---
+++ 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Result", "Text");
+    }
+}
