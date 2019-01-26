701cd9183bb76d04d55831836aa11bbcb71edfd1
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-01-26 18:29:49 +0200

    [MVC.05] Added examples of using *RedirectResult

--- 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
+++ 02.ASP.NET-Core/05.ControllersAndActions/ControllersAndActions/Controllers/HomeController.cs
@@ -16,5 +16,23 @@ namespace ControllersAndActions.Controllers
             ViewBag.Date = DateTime.Now;
             return View("Result");
         }
+
+        // NOTE: Each *Redirect method can has Permanent suffix which produces 301 status code
+        //       (permanent redirect, which can be cached by the browsers). Otherwise 302 temporary
+        //       redirect is produced.
+        // WARNING: You can not specify nameof(DerivedController) as controller paramenter for,
+        //          RedirectToAction because MVC will search DerivedControllerController class and
+        //          there is no suchcontroller
+        public RedirectResult RedirectExample() => Redirect("https://www.google.com");
+        public LocalRedirectResult LocalRedirectExample() => LocalRedirect("/Home/Index");
+        public RedirectToRouteResult RedirectToRouteExample() => 
+            RedirectToRoute(new {
+                controller = "Home",
+                action = "ReceiveForm",
+                name = "Alice",
+                city = "London"
+            });
+        public RedirectToActionResult RedirectToActionExample() =>
+            RedirectToAction(nameof(DerivedController.Headers), "Derived");
     }
 }
