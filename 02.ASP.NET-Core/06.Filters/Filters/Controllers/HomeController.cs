093cdc3e06635f1bbae1f8af3106c334313c15bb
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-01 22:39:57 +0200

    [MVC.06] Added Filters project

---
+++ 02.ASP.NET-Core/06.Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+            View("Message", "This is the Index action on the Home controller");
+
+        public ViewResult SecondAction() =>
+            View("Message", "This is the SecondAction action on the Home controller");
+    }
+}
---
+++ 02.ASP.NET-Core/06.Filters/Filters/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Filters
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
