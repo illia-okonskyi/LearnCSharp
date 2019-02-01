58f34ee871fd459dfe50b3492fba4977a70c64cb
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-01 22:51:13 +0200

    [MVC.06] Added HttpsOnlyAttribute as example of the authorization filter

--- 02.ASP.NET-Core/06.Filters/Filters/Controllers/HomeController.cs
+++ 02.ASP.NET-Core/06.Filters/Filters/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
+    // NOTE: Filters are applied as attributes to whole controller or to concrete action
+    //       To apply filter to all actions of all controllers use global filter
+
+    [HttpsOnly]
     public class HomeController : Controller
     {
         public ViewResult Index() =>
---
+++ 02.ASP.NET-Core/06.Filters/Filters/Infrastructure/HttpsOnlyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filters.Infrastructure
+{
+    // NOTE: async version of the interface is IAsyncAuthorizationFilter
+    public class HttpsOnlyAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.Request.IsHttps)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+        }
+    }
+}
