303a6b3ebedb26b8f1fa1a7b21a410860475ad9c
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-05 22:52:00 +0200

    [MVC.07] Added ContentController to provide examples of content formatting

---
+++ 02.ASP.NET-Core/07.ApiControllers/ApiControllers/Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using ApiControllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller
+    {
+        // NOTE: If the action method returns a string, the string is sent unmodified to the client,
+        // and the Content-Type header of the response is set to text/plain
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        // NOTE: Default behavior for all return types except string is to format it to JSON
+        [HttpGet("object")]
+        public Reservation GetObject() => new Reservation
+        {
+            Id = 100,
+            ClientName = "Joe",
+            Location = "Board Room"
+        };
+    }
+}
