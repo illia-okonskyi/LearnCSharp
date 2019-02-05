732b7e14f3753a628c9c82179a199ab1f3f53297
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-05 22:31:41 +0200

    [MVC.07] Added ReservationController as example of the REST controller

---
+++ 02.ASP.NET-Core/07.ApiControllers/ApiControllers/Controllers/ReservationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.JsonPatch;
+using ApiControllers.Models;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservationController : Controller
+    {
+        private IRepository _repository;
+
+        public ReservationController(IRepository repo) => _repository = repo;
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => _repository.Reservations;
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id) => _repository[id];
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+            _repository.AddReservation(new Reservation
+            {
+                ClientName = res.ClientName,
+                Location = res.Location
+            });
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) =>
+            _repository.UpdateReservation(res);
+
+        // NOTE: JSON Patch is a standard used to modify contents of the JSON document.
+        //       Reference: https://tools.ietf.org/html/rfc6902
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody]JsonPatchDocument<Reservation> patch)
+        {
+            Reservation res = Get(id);
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => _repository.DeleteReservation(id);
+    }
+}
