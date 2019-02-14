66ceee33308271f82debf5043aa4766c9695071a
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-14 22:37:35 +0200

    [MVC.09] String view component result replaced with view result of the view component, added notes on other view component result classes

--- 02.ASP.NET-Core/09.ViewComponents/ViewComponents/Components/CitySummary.cs
+++ 02.ASP.NET-Core/09.ViewComponents/ViewComponents/Components/CitySummary.cs
@@ -14,10 +14,55 @@ namespace ViewComponents.Components
             _repository = repository;
         }
 
-        public string Invoke()
+        // NOTE: Invoke method can return one of 4 result types:
+        //       1,2) string or ContentViewComponentResult (implements IViewComponentResult) -
+        //            result string will be safely HTML-encoded, use the Content method of the 
+        //            ViewComponent class
+        //       3) ViewViewComponentResult (implements IViewComponentResult) - This class is used
+        //          to specify a Razor view, with optional view model data. Use the View method of
+        //          the ViewComponent class
+        //       4) HtmlContentViewComponentResult (implements IViewComponentResult) - This class
+        //          is used to specify a fragment of HTML that will be included in the HTML
+        //          document without further encoding.There is no ViewComponent method to create
+        //          this type of result
+
+        //public string Invoke()
+        //{
+        //    return $"{_repository.Cities.Count()} cities, "
+        //        + $"{_repository.Cities.Sum(c => c.Population)} people";
+        //}
+        //
+        //
+        //public IViewComponentResult Invoke()
+        //{
+        //    return Content("This is a <h3><i>string</i></h3>");
+        //}
+        // 
+        //
+        //using Microsoft.AspNetCore.Mvc.ViewComponents;
+        //using Microsoft.AspNetCore.Html;
+        //public IViewComponentResult Invoke()
+        //{
+        //    return new HtmlContentViewComponentResult(
+        //        new HtmlString("This is a <h3><i>string</i></h3>"));
+        //}
+
+        // NOTE: Search locations of the partial views for view components:
+        //       - /Views/{controller}/Components/{component}/{viewName} | Default.cshtml
+        //       - /Views/Shared/Components/{component}/{viewName} | Default.cshtml
+        //       Sharing partial views to view compoments can be made by specifying full path
+        //       of the target view like Views/Components/CommonViews/ViewName.cshtml
+        public IViewComponentResult Invoke()
         {
-            return $"{_repository.Cities.Count()} cities, "
-                + $"{_repository.Cities.Sum(c => c.Population)} people";
+            var target = RouteData.Values["id"] as string;
+            var cities = _repository.Cities.Where(
+                city => target == null || string.Compare(city.Country, target, true) == 0);
+
+            return View(new CityViewModel
+            {
+                Cities = cities.Count(),
+                Population = cities.Sum(c => c.Population)
+            }); ;
         }
     }
 }
---
+++ 02.ASP.NET-Core/09.ViewComponents/ViewComponents/Models/CityViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewComponents.Models
+{
+    public class CityViewModel
+    {
+        public int Cities { get; set; }
+        public int Population { get; set; }
+    }
+}
