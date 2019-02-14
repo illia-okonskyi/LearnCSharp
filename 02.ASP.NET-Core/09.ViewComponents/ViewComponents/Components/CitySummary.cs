246b797f5dbccfcba66d9a70bb563d0b4380ddaa
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-14 22:00:51 +0200

    [MVC.09] POCO view component replaced with MVC class based view component

---
+++ 02.ASP.NET-Core/09.ViewComponents/ViewComponents/Components/CitySummary.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ViewComponents.Models;
+
+namespace ViewComponents.Components
+{
+    // NOTE: View components derived from ViewComponent can omit the "ViewComponent" prefix
+    public class CitySummary : ViewComponent
+    {
+        private readonly ICityRepository _repository;
+
+        public CitySummary(ICityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public string Invoke()
+        {
+            return $"{_repository.Cities.Count()} cities, "
+                + $"{_repository.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
