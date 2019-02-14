05335df4f1ed83bbe8a5fd38f41953df7684d04a
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-14 23:15:00 +0200

    [MVC.09] Added example of hybrid controller/view component

---
+++ 02.ASP.NET-Core/09.ViewComponents/ViewComponents/Controllers/CityController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using ViewComponents.Models;
+
+namespace ViewComponents.Controllers
+{
+    [ViewComponent(Name = "ComboComponent")]
+    public class CityController : Controller
+    {
+        private readonly ICityRepository _repository;
+
+        public CityController(ICityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City newCity)
+        {
+            _repository.AddCity(newCity);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IViewComponentResult Invoke() => new ViewViewComponentResult()
+        {
+            ViewData = new ViewDataDictionary<IEnumerable<City>>(ViewData, _repository.Cities)
+        };
+    }
+}
