1bd293c73cc4371152ba0a5cf63c8e3605d99291
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-14 22:56:26 +0200

    [MVC.09] Added example of async view component

---
+++ 02.ASP.NET-Core/09.ViewComponents/ViewComponents/Components/PageSize.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.Components
+{
+    public class PageSize : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://apress.com");
+            return View(response.Content.Headers.ContentLength);
+        }
+    }
+}
