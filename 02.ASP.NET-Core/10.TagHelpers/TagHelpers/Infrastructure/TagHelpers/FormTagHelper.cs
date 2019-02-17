8e1effad704b51a738cdb5aa0c4866245cc26a01
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-18 00:43:16 +0200

    [MVC.10] Added FormTagHelper as example of the tag helpers which uses DI

---
+++ 02.ASP.NET-Core/10.TagHelpers/TagHelpers/Infrastructure/TagHelpers/FormTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.Infrastructure.TagHelpers
+{
+    public class FormTagHelper : TagHelper
+    {
+        private IUrlHelperFactory _urlHelperFactory;
+
+        public FormTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContextData { get; set; }
+
+        public string Controller { get; set; }
+        public string Action { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContextData);
+            output.Attributes.SetAttribute(
+                "action",
+                urlHelper.Action(
+                    Action ?? ViewContextData.RouteData.Values["action"].ToString(),
+                    Controller ?? ViewContextData.RouteData.Values["controller"].ToString()
+                    ));
+        }
+    }
+}
