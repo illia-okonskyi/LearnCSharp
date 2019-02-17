232b25faa937e43e02c0beaa909f5b46fc21f4c3
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-17 23:08:48 +0200

    [MVC.10] Added TagHelpers project

---
+++ 02.ASP.NET-Core/10.TagHelpers/TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TagHelpers.Models;
+
+namespace TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repository;
+
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index() => View(_repository.Cities);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City city)
+        {
+            _repository.AddCity(city);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
