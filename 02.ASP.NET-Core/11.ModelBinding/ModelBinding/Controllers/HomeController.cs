17f7e3b254df514075a1dbe23a6a0f82d1cddb00
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-21 21:19:03 +0200

    [MVC.11] Added ModelBinding project

---
+++ 02.ASP.NET-Core/11.ModelBinding/ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public HomeController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound();
+            }
+
+            var person = _repository[id.Value];
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+    }
+}
---
+++ 02.ASP.NET-Core/11.ModelBinding/ModelBinding/Models/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace ModelBinding.Models
+{
+    public interface IRepository
+    {
+        IEnumerable<Person> People { get; }
+        Person this[int id] { get; set; }
+    }
+
+    public class MemoryRepository : IRepository
+    {
+        private Dictionary<int, Person> _people = new Dictionary<int, Person>
+        {
+            [1] = new Person
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Smith",
+                Role = Role.Admin
+            },
+            [2] = new Person
+            {
+                Id = 2,
+                FirstName = "Anne",
+                LastName = "Douglas",
+                Role = Role.User
+            },
+            [3] = new Person
+            {
+                Id = 3,
+                FirstName = "Joe",
+                LastName = "Able",
+                Role = Role.User
+            },
+            [4] = new Person
+            {
+                Id = 4,
+                FirstName = "Mary",
+                LastName = "Peters",
+                Role = Role.Guest
+            }
+        };
+
+        public IEnumerable<Person> People => _people.Values;
+
+        public Person this[int id]
+        {
+            get => _people.ContainsKey(id) ? _people[id] : null;
+            set => _people[id] = value;
+        }
+    }
+}
