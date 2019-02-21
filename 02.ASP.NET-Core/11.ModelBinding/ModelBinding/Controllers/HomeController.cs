8c85ef9e80d370134e45eaf14e90696ad262f406
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-21 22:27:42 +0200

    [MVC.11] Added example of binding values of the complex objects

--- 02.ASP.NET-Core/11.ModelBinding/ModelBinding/Controllers/HomeController.cs
+++ 02.ASP.NET-Core/11.ModelBinding/ModelBinding/Controllers/HomeController.cs
@@ -28,5 +28,34 @@ namespace ModelBinding.Controllers
 
             return View(person);
         }
+
+        public ViewResult Create() => View(new Person());
+
+        [HttpPost]
+        public ViewResult Create(Person model) => View("Index", model);
+
+
+        // NOTE: The Bind attribute can be used to bind some arguments with specified prefix.
+        //       Note that only prefix can be specified, the names of the properties must be same
+        //       as class has.
+        //       BindNever attribute (can be applied to the property) tells MVC do not bind any
+        //       value from  the view to the model.
+        //       BindRequired attribute that tells the model  binding process that a request must
+        //       include a value for a property. If the request doesn’t have a required value, then
+        //       a model validation error is produced. Example:
+        //
+        //public class Models.AddressSummary
+        //{
+        //    [BindRequired]
+        //    public string City { get; set; }
+        //    [BindNever]
+        //    public string Country { get; set; }
+        //}
+        //[HttpPost]
+        //public ViewResult DisplayAddressSummary(
+        //    [Bind(nameof(AddressSummary.City), Prefix = nameof(Person.HomeAddress))] AddressSummary summary)
+        //{
+        //    return View(summary);
+        //}
     }
 }
