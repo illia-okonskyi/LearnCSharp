3bab6b32353ab005e2cf3569431372d4628fd8f7
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-21 22:47:28 +0200

    [MVC.11] Added example of binding collections of complex types

--- 02.ASP.NET-Core/11.ModelBinding/ModelBinding/Controllers/HomeController.cs
+++ 02.ASP.NET-Core/11.ModelBinding/ModelBinding/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ModelBinding.Models;
 
@@ -57,5 +58,8 @@ namespace ModelBinding.Controllers
         //{
         //    return View(summary);
         //}
+
+        public ViewResult Addresses(IList<AddressSummary> addresses) =>
+            View(addresses ?? new List<AddressSummary>());
     }
 }
