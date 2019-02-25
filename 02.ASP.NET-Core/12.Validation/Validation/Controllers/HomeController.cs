681c35c8941b2113801df79b9feaf573fce31bf0
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-25 21:41:29 +0200

    [MVC.12] Added Validation project

---
+++ 02.ASP.NET-Core/12.Validation/Validation/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() =>
+            View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) =>
+            View("Completed", appt);
+    }
+}
