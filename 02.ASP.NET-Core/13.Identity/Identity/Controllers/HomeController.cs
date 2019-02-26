bfa5a479fba1be0b66d85c4658a51005739b2270
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-26 20:21:14 +0200

    [MVC.13] Added Identity project

---
+++ 02.ASP.NET-Core/13.Identity/Identity/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+            View(new Dictionary<string, object> {
+                ["Placeholder"] = "Placeholder"
+            });
+    }
+}
