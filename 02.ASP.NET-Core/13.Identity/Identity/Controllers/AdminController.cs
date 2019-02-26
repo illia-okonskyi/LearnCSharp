ae22958014f32f09cb63b191715da3e14d105b58
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-02-26 22:41:55 +0200

    [MVC.13] Added editing and deleting existing users functionality

--- 02.ASP.NET-Core/13.Identity/Identity/Controllers/AdminController.cs
+++ 02.ASP.NET-Core/13.Identity/Identity/Controllers/AdminController.cs
@@ -8,9 +8,19 @@ namespace Identity.Controllers
     public class AdminController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
-        public AdminController(UserManager<AppUser> userManager)
+        private readonly IUserValidator<AppUser> _userValidator;
+        private readonly IPasswordValidator<AppUser> _passwordValidator;
+        private readonly IPasswordHasher<AppUser> _passwordHasher;
+        public AdminController(
+            UserManager<AppUser> userManager,
+            IUserValidator<AppUser> userValidator,
+            IPasswordValidator<AppUser> passwordValidator,
+            IPasswordHasher<AppUser> passwordHasher)
         {
             _userManager = userManager;
+            _userValidator = userValidator;
+            _passwordValidator = passwordValidator;
+            _passwordHasher = passwordHasher;
         }
 
         public ViewResult Index() => View(_userManager.Users);
@@ -42,6 +52,91 @@ namespace Identity.Controllers
             }
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email, string password)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "User Not Found");
+                return View(user);
+            }
+
+            user.Email = email;
+            var userValidationResult = await _userValidator.ValidateAsync(_userManager, user);
+            if (!userValidationResult.Succeeded)
+            {
+                AddErrorsFromResult(userValidationResult);
+            }
+
+            if (!string.IsNullOrEmpty(password))
+            {
+                var passwordValidationResult = await _passwordValidator.ValidateAsync(_userManager,
+                    user, password);
+                if (passwordValidationResult.Succeeded)
+                {
+                    user.PasswordHash = _passwordHasher.HashPassword(user, password);
+                }
+                else
+                {
+                    AddErrorsFromResult(passwordValidationResult);
+                }
+            }
+
+            if (ModelState.ErrorCount != 0)
+            {
+                return View(user);
+            }
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                AddErrorsFromResult(result);
+                return View(user);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "User Not Found");
+                return View(nameof(Index), _userManager.Users);
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                AddErrorsFromResult(result);
+                return View(nameof(Index), _userManager.Users);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
         }
     }
 }
