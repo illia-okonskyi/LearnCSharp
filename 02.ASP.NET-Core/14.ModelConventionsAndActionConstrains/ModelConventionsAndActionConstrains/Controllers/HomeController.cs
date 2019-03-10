d1fe32804837fb5d21558e847320b4aa442f37bb
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-03-10 23:35:23 +0200

    [MVC.14] Added ModelConventionsAndActionConstrains project

---
+++ 02.ASP.NET-Core/14.ModelConventionsAndActionConstrains/ModelConventionsAndActionConstrains/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelConventionsAndActionConstrains.Models;
+
+namespace ModelConventionsAndActionConstrains.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+
+        public IActionResult List() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(List)
+        });
+    }
+}
---
+++ 02.ASP.NET-Core/14.ModelConventionsAndActionConstrains/ModelConventionsAndActionConstrains/Models/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace ModelConventionsAndActionConstrains.Models
+{
+    public class Result
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
