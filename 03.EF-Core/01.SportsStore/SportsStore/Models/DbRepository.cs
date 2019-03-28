1ca7596a3cd66183f650fde56436268cf77e708d
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-03-28 21:25:24 +0200

    [EF.01] Force cast DbSet to List is applied to avoid multiple DB queries

--- 03.EF-Core/01.SportsStore/SportsStore/Models/DbRepository.cs
+++ 03.EF-Core/01.SportsStore/SportsStore/Models/DbRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsStore.Models
 {
@@ -8,7 +9,13 @@ namespace SportsStore.Models
 
         public DbRepository(AppDbContext dbContext) => _dbContext = dbContext;
 
-        public IEnumerable<Product> Products => _dbContext.Products;
+        // NOTE: Force cast DbSet to List (or Array) can be used to avoid multiple DB queries when
+        //       LINQ is used to operate data. But it can be bad practice in real applications
+        //       because all the table entries are loaded into memory. In real applications
+        //       it is still usefull to avoid multiple DB queries and operate on memory data, but
+        //       more complex approach must be used, which can include query builder, pagination,
+        //       etc.
+        public IEnumerable<Product> Products => _dbContext.Products.ToList();
 
         public void AddProduct(Product product)
         {
