f336273b15739c0ffb031582abc3d6f31e53d4d7
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-04-23 21:54:07 +0300

    [EF.01] Added StoreController

---
+++ 03.EF-Core/01.SportsStore/SportsStore/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.Pages;
+
+namespace SportsStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly IRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public StoreController(
+            IRepository productRepository,
+            ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index(
+            [FromQuery(Name = "options")] QueryOptions productOptions,
+            QueryOptions categoryOptions,
+            long categoryId)
+        {
+            ViewBag.Categories = _categoryRepository.GetCategories(categoryOptions);
+            ViewBag.SelectedCategoryId = categoryId;
+            return View(_productRepository.GetProducts(productOptions, categoryId));
+        }
+    }
+}
