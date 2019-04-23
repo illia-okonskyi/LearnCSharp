c34bda65d31aa2166f012ff6c50e5838faa3032e
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-04-23 21:16:19 +0300

    [EF.01] Extended Product and Category models

--- 03.EF-Core/01.SportsStore/SportsStore/Models/Product.cs
+++ 03.EF-Core/01.SportsStore/SportsStore/Models/Product.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public long CategoryId { get; set; }
         public Category Category { get; set; }
         public decimal PurchasePrice { get; set; }
