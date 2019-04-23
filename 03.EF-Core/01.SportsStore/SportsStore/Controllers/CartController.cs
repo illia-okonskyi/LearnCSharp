05ccd9c132126fdd79d00e350e5007cfcbf5b9b0
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-04-23 22:28:33 +0300

    [EF.01] Added cart feature

---
+++ 03.EF-Core/01.SportsStore/SportsStore/Controllers/CartController.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Http;
+using SportsStore.Models;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Controllers
+{
+    [ViewComponent(Name = "Cart")]
+    public class CartController : Controller
+    {
+        private readonly IOrdersRepository _ordersRepository;
+        public CartController(IOrdersRepository ordersRepository)
+        {
+            _ordersRepository = ordersRepository;
+        }
+
+        public IActionResult Index(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View(GetCart());
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(Product product, string returnUrl)
+        {
+            SaveCart(GetCart().AddItem(product, 1));
+            return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveFromCart(long productId, string returnUrl)
+        {
+            SaveCart(GetCart().RemoveItem(productId));
+            return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        public IActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            order.Lines = GetCart().Selections.Select(s => new OrderLine {
+                ProductId = s.ProductId,
+                Quantity = s.Quantity
+            }).ToArray();
+            _ordersRepository.AddOrder(order);
+
+            SaveCart(new Cart());
+            return RedirectToAction(nameof(Completed));
+        }
+        public IActionResult Completed() => View();
+
+        private Cart GetCart() => HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+
+        private void SaveCart(Cart cart) => HttpContext.Session.SetJson("cart", cart);
+
+        public IViewComponentResult Invoke(ISession session)
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<Cart>(ViewData, session.GetJson<Cart>("cart"))
+            };
+        }
+    }
+}
---
+++ 03.EF-Core/01.SportsStore/SportsStore/Models/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class Cart
+    {
+        private readonly List<OrderLine> _selections = new List<OrderLine>();
+
+        public IEnumerable<OrderLine> Selections { get => _selections; }
+
+        public Cart AddItem(Product product, int quantity)
+        {
+            var line = _selections.Where(l => l.ProductId == product.Id).FirstOrDefault();
+            if (line != null)
+            {
+                line.Quantity += quantity;
+                return this;
+            }
+
+            _selections.Add(new OrderLine {
+                ProductId = product.Id,
+                Product = product,
+                Quantity = quantity
+            });
+            return this;
+        }
+
+        public Cart RemoveItem(long productId)
+        {
+            _selections.RemoveAll(l => l.ProductId == productId);
+            return this;
+        }
+
+        public void Clear() => _selections.Clear();
+    }
+}
