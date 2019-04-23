def32099ae9611de1209ee849b202da1fdece553
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-04-23 21:53:49 +0300

    [EF.01] Added return-URL resolver

---
+++ 03.EF-Core/01.SportsStore/SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest request)
+        {
+            if (!request.QueryString.HasValue)
+            {
+                return request.Path.ToString();
+            }
+
+            return $"{request.Path}{request.QueryString}";
+        }
+    }
+}
