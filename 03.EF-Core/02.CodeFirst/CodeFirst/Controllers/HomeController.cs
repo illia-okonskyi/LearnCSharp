2b50fe5c54f289108f544bd55780bad3c06c5a0e
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-05-07 21:18:58 +0300

    [EF.02] Added CodeFirst project

---
+++ 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeFirst.Models;
+
+namespace CodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Product[]
+            {
+                new Product { Name = "P1", Category = "Cat1", Price = 10 },
+                new Product { Name = "P2", Category = "Cat2", Price = 20 },
+                new Product { Name = "P3", Category = "Cat3", Price = 30 },
+            });
+        }
+    }
+}
---
+++ 03.EF-Core/02.CodeFirst/CodeFirst/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeFirst.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
