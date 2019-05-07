26a5282243f93401e1ec95ab678f0bb48cdce328
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-05-07 22:09:47 +0300

    [EF.02] Added missing CRUD actions to the Home controller and views

--- 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/HomeController.cs
+++ 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/HomeController.cs
@@ -16,5 +16,37 @@ namespace CodeFirst.Controllers
         {
             return View(_productsRepository.GetAllProducts());
         }
+
+        public IActionResult Create()
+        {
+            ViewBag.CreateMode = true;
+            return View("Editor", new Product());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _productsRepository.CreateProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(long id)
+        {
+            ViewBag.CreateMode = false;
+            return View("Editor", _productsRepository.GetProduct(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            _productsRepository.UpdateProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(long id)
+        {
+            _productsRepository.DeleteProduct(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
