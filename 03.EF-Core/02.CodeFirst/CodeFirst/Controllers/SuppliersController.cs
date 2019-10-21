f7acac3d5ed5979db78fc90d16e1d67a80f82559
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-10-21 20:47:15 +0300

    [EF.02] Added example of changing the relation from the other side of the 1-to-many relation

--- 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/SuppliersController.cs
+++ 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/SuppliersController.cs
@@ -1,5 +1,7 @@
-﻿using CodeFirst.Models;
+﻿using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CodeFirst.Models;
 
 namespace CodeFirst.Controllers
 {
@@ -14,6 +16,7 @@ namespace CodeFirst.Controllers
         {
             ViewBag.SupplierEditId = TempData["SupplierEditId"];
             ViewBag.SupplierCreateId = TempData["SupplierCreateId"];
+            ViewBag.SupplierRelationshipId = TempData["SupplierRelationshipId"];
             return View(_supplierRepository.GetAll());
         }
 
@@ -31,6 +34,40 @@ namespace CodeFirst.Controllers
         public IActionResult Update(Supplier supplier)
         {
             _supplierRepository.Update(supplier);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Change(long id)
+        {
+            TempData["SupplierRelationshipId"] = id;
+            return RedirectToAction(nameof(Index));
+        }
+
+        // This approach works-around EF Core change detection system
+        // The more siplier way of doing this is to use direct direction of the one-to-many
+        // relation like this:
+        // public IActionResult Change(long Id, Product[] products)
+        // {
+        //     dbContext.Products.UpdateRange(products.Where(p => p.SupplierId != Id));
+        //     dbContext.SaveChanges();
+        //     return RedirectToAction(nameof(Index));
+        // }
+        [HttpPost]
+        public IActionResult Change(Supplier supplier)
+        {
+            var changedProducts = supplier.Products.Where(p => p.SupplierId != supplier.Id);
+            if (changedProducts.Count() > 0)
+            {
+                var allSuppliers = _supplierRepository.GetAll().ToArray();
+                var currentSupplier = allSuppliers.First(s => s.Id == supplier.Id);
+                foreach (var p in changedProducts)
+                {
+                    var newSupplier = allSuppliers.First(s => s.Id == p.SupplierId);
+                    newSupplier.Products = newSupplier.Products
+                        .Append(currentSupplier.Products.First(op => op.Id == p.Id)).ToArray();
+                    _supplierRepository.Update(newSupplier);
+                }
+            }
             return RedirectToAction(nameof(Index));
         }
     }
