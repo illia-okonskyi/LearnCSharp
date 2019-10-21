d42977783a611a2aa7a6e110d4ef8a68ab845b6a
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-10-21 19:59:31 +0300

    [EF.02] Added example of create, read and update operations from the CRUD functionality from the other side of the 1-to-many relation

---
+++ 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/SuppliersController.cs
@@ -0,0 +1,37 @@
+﻿using CodeFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirst.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private ISupplierRepository _supplierRepository;
+        public SuppliersController(ISupplierRepository supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.SupplierEditId = TempData["SupplierEditId"];
+            ViewBag.SupplierCreateId = TempData["SupplierCreateId"];
+            return View(_supplierRepository.GetAll());
+        }
+
+        public IActionResult Edit(long id)
+        {
+            TempData["SupplierEditId"] = id;
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Create(long id)
+        {
+            TempData["SupplierCreateId"] = id;
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public IActionResult Update(Supplier supplier)
+        {
+            _supplierRepository.Update(supplier);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
