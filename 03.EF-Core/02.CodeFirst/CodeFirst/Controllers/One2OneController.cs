3b07b5bd21862e78edb42ed1a618777da075aeeb
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-10-22 17:15:15 +0300

    [EF.02] Added code required for swapping the 1-to-1 relation

--- 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/One2OneController.cs
+++ 03.EF-Core/02.CodeFirst/CodeFirst/Controllers/One2OneController.cs
@@ -19,6 +19,7 @@ namespace CodeFirst.Controllers
         public IActionResult Create() => View("ContactEditor");
         public IActionResult Edit(long id)
         {
+            ViewBag.Suppliers = _dbContext.Suppliers.Include(s => s.Contact);
             return View("ContactEditor",
                 _dbContext.Set<ContactDetails>()
                     .Include(cd => cd.Supplier)
@@ -27,7 +28,10 @@ namespace CodeFirst.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(ContactDetails details)
+        public IActionResult Update(
+            ContactDetails details,
+            long? targetSupplierId,
+            long[] spares)
         {
             if (details.Id == 0)
             {
@@ -36,6 +40,47 @@ namespace CodeFirst.Controllers
             else
             {
                 _dbContext.Update<ContactDetails>(details);
+                if (targetSupplierId.HasValue)
+                {
+                    if (spares.Contains(targetSupplierId.Value))
+                    {
+                        details.SupplierId = targetSupplierId.Value;
+                    }
+                    else
+                    {
+                        // NOTE: For swapping the relation in 1-to-1 relationships 4 steps are
+                        //       required
+                        //       1) old = current, unique is broken
+                        //       2) current = temp.FK_Id = create new primary object (with id e.g.
+                        //          15), unique is fixed
+                        //       - save changes
+                        //       3) current = old, unique is fixed
+                        //       4) remove temp primary object (id = 15)
+                        //       - save changes
+                        // var targetDetails = _dbContext.Set<ContactDetails>()
+                        //     .FirstOrDefault(cd => cd.SupplierId == targetSupplierId);
+                        // targetDetails.SupplierId = details.Supplier.Id;
+                        // Supplier temp = new Supplier { Name = "temp" };
+                        // details.Supplier = temp;
+                        // _dbContext.SaveChanges();
+                        // details.SupplierId = targetSupplierId.Value;
+                        // temp.Contact = null;
+                        // _dbContext.Suppliers.Remove(temp);
+
+                        // NOTE: Work with 1-to-0|1 relation is much simpler because there is no
+                        //       unique foreign key requirement
+
+                        var targetDetails = _dbContext.Set<ContactDetails>()
+                            .FirstOrDefault(cd => cd.SupplierId == targetSupplierId);
+                        targetDetails.SupplierId = details.Supplier.Id;
+                        Supplier temp = new Supplier { Name = "temp" };
+                        details.Supplier = temp;
+                        _dbContext.SaveChanges();
+                        details.SupplierId = targetSupplierId.Value;
+                        temp.Contact = null;
+                        _dbContext.Suppliers.Remove(temp);
+                    }
+                }
             }
             _dbContext.SaveChanges();
             return RedirectToAction(nameof(Index));
