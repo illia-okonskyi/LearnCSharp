8847d8af918a62748d1a509889c6d2d16179faaf
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-10-25 16:10:18 +0300

    [EF.04[ Added example which demonstrates differences on translating the LINQ queries to SQL depending on accessiblity of the query code for the EF Core

---
+++ 03.EF-Core/04.AdvancedApp/AdvancedApp/Controllers/QueryController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AdvancedApp.Models;
+
+namespace AdvancedApp.Controllers
+{
+    public class QueryController : Controller
+    {
+        private AdvancedContext _context;
+        public QueryController(AdvancedContext context) => _context = context;
+
+        public IActionResult ServerEval()
+        {
+            // Produces query:
+            //   SELECT[e].[SSN], [e].[FirstName], [e].[FamilyName], [e].[Salary], [e].[SoftDeleted]
+            //   FROM[Employees] AS[e]
+            //   WHERE([e].[SoftDeleted] = 0) AND([e].[Salary] > 150000.0)
+            // And no local(client) calculations
+            return View("Query", _context.Employees.Where(e => e.Salary > 150_000));
+        }
+
+        public IActionResult ClientEval()
+        {
+            // Produces query:
+            //   SELECT[e].[SSN], [e].[FirstName], [e].[FamilyName], [e].[Salary], [e].[SoftDeleted]
+            //   FROM[Employees] AS[e]
+            //   WHERE([e].[SoftDeleted] = 0)
+            // And IsHighEarner(e) is calculated locally, the log warning message appears:
+            //   The LINQ expression 'where value(AdvancedApp.Controllers.QueryController)
+            //   .IsHighEarner([e])' could not be translated and will be evaluated locally
+            // To throw exception when such warning appears additional configuration of the
+            // DbContext is needed like this (Startup.cs):
+            // services.AddDbContext<AdvancedContext>(options =>
+            //     options.UseSqlServer(connectionString).ConfigureWarnings(warning =>
+            //         warning.Throw(RelationalEventId.QueryClientEvaluationWarning))
+            // );
+            return View("Query", _context.Employees.Where(e => IsHighEarner(e)));
+        }
+
+        private bool IsHighEarner(Employee e)
+        {
+            return e.Salary > 150_000;
+        }
+    }
+}
--- 03.EF-Core/04.AdvancedApp/AdvancedApp/Startup.cs
+++ 03.EF-Core/04.AdvancedApp/AdvancedApp/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using AdvancedApp.Models;
 
 namespace AdvancedApp
@@ -16,8 +17,21 @@ namespace AdvancedApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // NOTE: warning messages can be configured for service via WarningsConfigurationBuilder:
+            //       - Ignore(event) - This method tells Entity Framework Core to ignore the
+            //                         specified event.
+            //       - Log(event) - This method tells Entity Framework Core to log the specified
+            //                      event.
+            //       - Throw(event) - This method tells Entity Framework Core to throw an exception
+            //                        for the specified event.
+            //      Complete list of the events is available at
+            //      https://docs.microsoft.com/en-us/ef/core/api/microsoft.entityframeworkcore.infrastructure.relationaleventid
             string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
-            services.AddDbContext<AdvancedContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<AdvancedContext>(options =>
+                options.UseSqlServer(connectionString).ConfigureWarnings(warning =>
+                    warning.Throw(RelationalEventId.QueryClientEvaluationWarning))
+            );
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
