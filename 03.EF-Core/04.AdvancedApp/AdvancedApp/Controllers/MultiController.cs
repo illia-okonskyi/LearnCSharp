001aa6f8fc6fd5436b842ae94a39c83c36a9af10
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-10-29 17:13:56 +0200

    [EF.04] Prepared code for next chapter in the book

---
+++ 03.EF-Core/04.AdvancedApp/AdvancedApp/Controllers/MultiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdvancedApp.Models;
+
+namespace AdvancedApp.Controllers
+{
+    public class MultiController : Controller
+    {
+        private readonly AdvancedContext _context;
+        private readonly ILogger<MultiController> _logger;
+        public MultiController(AdvancedContext context, ILogger<MultiController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("EditAll", _context.Employees);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateAll(Employee[] employees)
+        {
+            _context.UpdateRange(employees);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
